db5867f56553491728510f7ac47327e277f9f4b3
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-06-25 00:49:18 -0400

    left at custom hooks

--- LL-React-Essential-Training/Ex Files/Ch05/05_01/start/react-app/src/App.js
+++ LL-React-Essential-Training/Ex Files/Ch05/05_01/start/react-app/src/App.js
@@ -1,23 +1,40 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useState } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
-  return (
-    <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
-  );
+	const [title, setTitle] = useState("");
+	const [color, setColor] = useState("#000000");
+	const submit = (e) => {
+		// Prevent the default form behavior
+		e.preventDefault();
+
+		// Get the value of the input fields
+		// We are listening to the onChange event of the input fields so we don't need to
+
+		// Do something with it
+		console.log(title, color);
+
+		// Clear the form
+		setTitle("");
+		setColor("#000000");
+	};
+
+	return (
+		<form onSubmit={submit}>
+			<input
+				onChange={(event) => setTitle(event.target.value)}
+				value={title}
+				type="text"
+				placeholder="Color title"
+			/>
+			<input
+				onChange={(event) => setColor(event.target.value)}
+				value={color}
+				type="color"
+			/>
+			<button>Add</button>
+		</form>
+	);
 }
 
 export default App;

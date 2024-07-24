969db4726c98720fb479887668a2f178ef3c8d13
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-07-23 22:02:35 -0400

    after tailwind is installed

--- LL-React-Building-an-Interface/my-code/ch1/reactinterface/src/App.js
+++ LL-React-Building-an-Interface/my-code/ch1/reactinterface/src/App.js
@@ -2,9 +2,10 @@ import { BiAlarm } from "react-icons/bi";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>
-				<BiAlarm /> Your appointments
+		<div className="App container mx-auto mt-40 font-thin">
+			<h1 className="text-5xl">
+				<BiAlarm className="inline-block text-red-600" /> Your
+				Appointments
 			</h1>
 		</div>
 	);

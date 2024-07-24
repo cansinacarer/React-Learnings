abb278884136b4a29b7872c7304f6cce2cda4bb2
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-07-23 21:20:12 -0400

    imported icons, not yet tailwind

---
+++ LL-React-Building-an-Interface/my-code/ch1/reactinterface/src/App.js
@@ -0,0 +1,13 @@
+import { BiAlarm } from "react-icons/bi";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>
+				<BiAlarm /> Your appointments
+			</h1>
+		</div>
+	);
+}
+
+export default App;

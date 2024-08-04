664e347c9c32a80ed7ade7268daaa08f380d2598
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-08-04 01:08:01 -0400

    playing around

--- LL-React-Essential-Training/ex-files/Ch06/06_01/finished/react-app/src/App.js
+++ LL-React-Essential-Training/ex-files/Ch06/06_01/finished/react-app/src/App.js
@@ -3,18 +3,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState(null);
+  const [parsedData, setParsedData] = useState(null);
+
   useEffect(() => {
     fetch(
-      `https://api.github.com/users/moonhighway`
+      `https://api.github.com/users/cansinacarer`
     )
       .then((response) => response.json())
-      .then(setData);
+      .then(setData) // short for .then(data => setData(data))
   }, []);
-  if (data)
-    return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    );
-  return <h1>Data</h1>;
+
+return (
+        <div>
+                <h1>Data</h1>
+                {data && (
+                    <>
+                        <div style={{ textAlign: "center" }}>
+                            <img src={data.avatar_url} alt="" height="160" width="160" />
+                            <p><a href={data.html_url} target="_blank">{data.login}</a></p>
+                        </div>
+                        <pre>{JSON.stringify(data, null, 2)}</pre>
+                    </>
+                )}
+                {!data && (
+                    <p>No data available</p>
+                )}
+        </div>
+)
 }
 
 export default App;
